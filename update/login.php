4113abbeca3ee6517ba63d3df976d0bee66cb0e4
Erik <eriklab1222@gmail.com>
2017-11-06 23:58:31 -0800

    added added sessions on to login and login attempt timeout

--- update/login.php
+++ update/login.php
@@ -1,5 +1,27 @@
 <link rel="stylesheet" href="css/common.css">
-
+<?php
+$thisTime = time();
+session_start();
+$_SESSION['loginAttempt'] = 0;
+$_SESSION['authenticated'];
+$_SESSION['username'];
+if ($_SESSION['authenticated'] == 1)
+{
+        header("location: index.php");
+}
+if($_SESSION['loginAttempt'] == 5)
+{
+        $timeNow = time();
+        if (timeNow >= ($thisTime + 30))
+        {
+          $_SESSION['loginAttempt'] = 0;
+        }
+        else
+        {
+          die("you are frozen");
+        }
+}
+?>
 <html>
 <!-- ==================Start Header============================ -->
 <meta charset="UTF-8">
@@ -11,17 +33,20 @@
 <!-- ====================Start Body========================== -->
 <center>
 
-	<form action="index.php">
-	    <button style="width:35%">Home</button><br>
-	</form>
+        <form action="index.php">
+            <button style="width:35%">Home</button><br>
+        </form>
     <br>
-	<form action="login_results.php" method="post">
-	<div class="login">
-		<input type="text" placeholder="userName" name="userId" id="userId"><br>
-		<input type="password" placeholder="password" name="pswrd" id="pswrd"><br>
-		<input type="submit"/>
-	</div>
-	</form>
+        <form action="login_results.php" method="post">
+        <div class="login">
+                <input type="text" placeholder="userName" name="userId" id="userId"><br>
+                <input type="password" placeholder="password" name="pswrd" id="pswrd"><br>
+                <input type="submit"/>
+        </div>
+        <?php
+              echo $_SESSION['errormessage'];
+        ?>
+        </form>
 </center>
 <footer> Mortalwombat Team</footer>
 </body>
