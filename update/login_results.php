f381ea3d1471cf7afb403cddbe27651ab6d3937c
Erik <eriklab1222@gmail.com>
2017-11-07 12:36:35 -0800

    added comments on PDO and Parameterized statements

--- update/login_results.php
+++ update/login_results.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 $valid = array("macklin","admin", "guest","lampcougars","ligers","cyberlinux", "cybercrypts","mortalwombat","team6","team7", "test"); //array of valid user names
-$userId = strtolower($_POST['userId']);
+$userId = strtolower($_POST['userId']); //ensures that username is not case sensitive
 $password = $_POST['pswrd'];
-if (strlen($password) > 9 || strlen($userId) > 25)
+if (strlen($password) > 9 || strlen($userId) > 25) //validates length of input fields
 {
         $_SESSION['errormessage'] = "incorrect password or username";
         header("location: login.php");
@@ -12,6 +12,7 @@ $key = array_search($userId,$valid); //returns a key referencing location of val
 $found = $valid[$key]; // stores the valid username
 $mysqli = new PDO("mysql:dbname=team05;host=localhost", "team05", "passwordTEAM05mysql!") or die('Could not connect to the server!' . mysqli_error());
 
+//neccessary setup for PDO parameterized statements
 $mysqli -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $mysqli -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
